91a4b3aaf3856b86e239f42257495639cf19b523
Capamed <39675448+Capamed@users.noreply.github.com>
2023-09-11 00:50:51 -0500

    integration mobile

--- src/main.ts
+++ src/main.ts
@@ -2,12 +2,22 @@ import "reflect-metadata"
 import express from "express"
 import database from "./config/database";
 import indexRoutes from "./api/index";
+import cors from "cors";
 
 const app = express();
 
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
-app.use('/api',indexRoutes);
+
+// Configurar opciones CORS
+const corsOptions = {
+    origin: 'http://localhost:64617', // Reemplaza con el origen de tu aplicación web
+    methods: 'GET,HEAD,PUT,PATCH,POST,DELETE',
+    credentials: true, // Habilitar el uso de credenciales (cookies, autenticación, etc.)
+    optionsSuccessStatus: 204, // Establecer el código de estado para respuestas OPTIONS
+};
+app.use(cors(corsOptions))
+app.use('/api', indexRoutes);
 
 database.initialize()
     .then(() => {
